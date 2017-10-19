9cfdc0824579f706c8c48863d4167b6c5e0b7852
Laurence Roberts <lsjroberts@outlook.com>
2017-10-19 13:25:20 +0200

    Format with prettier

--- scripts/entry.js
+++ scripts/entry.js
@@ -1,5 +1,4 @@
-function Entry(data)
-{
+function Entry(data) {
   this.portal = data.portal ? data.portal : r.portal.data;
   this.message = data.message;
   this.quote = data.quote;
@@ -13,72 +12,130 @@ function Entry(data)
   this.seed = data.seed;
   this.whisper = data.whisper;
 
-  this.to_json = function()
-  {
-    return {message:this.message,timestamp:this.timestamp,editstamp:this.editstamp,media:this.media,target:this.target,ref:this.ref,quote:this.quote,whisper:this.whisper};
-  }
+  this.to_json = function() {
+    return {
+      message: this.message,
+      timestamp: this.timestamp,
+      editstamp: this.editstamp,
+      media: this.media,
+      target: this.target,
+      ref: this.ref,
+      quote: this.quote,
+      whisper: this.whisper
+    };
+  };
 
-  this.to_html = function()
-  {
+  this.to_html = function() {
     var html = "";
 
-    html += "<a href='"+this.dat+"'><img class='icon' src='"+this.dat+"/media/content/icon.svg'></a>";
+    html +=
+      "<a href='" +
+      this.dat +
+      "'><img class='icon' src='" +
+      this.dat +
+      "/media/content/icon.svg'></a>";
 
     html += this.header();
     html += this.body();
     html += this.rmc();
 
-    return "<div class='entry "+(this.whisper ? 'whisper' : '')+"'>"+html+"<hr/></div>";
-  }
+    return (
+      "<div class='entry " +
+      (this.whisper ? "whisper" : "") +
+      "'>" +
+      html +
+      "<hr/></div>"
+    );
+  };
 
-  this.header = function()
-  {
-    var html = ""
-
-    html += "<t class='portal'><a href='"+this.dat+"'>"+(this.seed ? "@" : "~")+this.portal+"</a> "+this.rune()+" "+(this.target ? "<a href='"+this.target+"'>"+portal_from_hash(this.target.toString())+"</a>" : "")+"</t>";
-
-    var operation = this.portal == r.portal.data.name ? 'edit:'+this.id+' '+this.message.replace(/\'/g,"&apos;") : "quote:"+this.portal+"-"+this.id+" ";
-
-    html += this.editstamp ? "<c class='editstamp' data-operation='"+operation+"'>edited "+timeSince(this.editstamp)+" ago</c>" : "<c class='timestamp' data-operation='"+operation+"'>"+timeSince(this.timestamp)+" ago</c>";
-
-    return html+"<hr />";
-  }
-
-  this.body = function()
-  {
+  this.header = function() {
     var html = "";
-    html += "<t class='message' dir='auto'>"+(this.formatter(this.message))+"</t><br/>";
-    if(this.quote){ html += "<t class='message quote' dir='auto'>"+(this.formatter(this.quote.message))+"</t><br/>"; }
+
+    html +=
+      "<t class='portal'><a href='" +
+      this.dat +
+      "'>" +
+      (this.seed ? "@" : "~") +
+      this.portal +
+      "</a> " +
+      this.rune() +
+      " " +
+      (this.target
+        ? "<a href='" +
+          this.target +
+          "'>" +
+          portal_from_hash(this.target.toString()) +
+          "</a>"
+        : "") +
+      "</t>";
+
+    var operation =
+      this.portal == r.portal.data.name
+        ? "edit:" + this.id + " " + this.message.replace(/\'/g, "&apos;")
+        : "quote:" + this.portal + "-" + this.id + " ";
+
+    html += this.editstamp
+      ? "<c class='editstamp' data-operation='" +
+        operation +
+        "'>edited " +
+        timeSince(this.editstamp) +
+        " ago</c>"
+      : "<c class='timestamp' data-operation='" +
+        operation +
+        "'>" +
+        timeSince(this.timestamp) +
+        " ago</c>";
+
+    return html + "<hr />";
+  };
+
+  this.body = function() {
+    var html = "";
+    html +=
+      "<t class='message' dir='auto'>" +
+      this.formatter(this.message) +
+      "</t><br/>";
+    if (this.quote) {
+      html +=
+        "<t class='message quote' dir='auto'>" +
+        this.formatter(this.quote.message) +
+        "</t><br/>";
+    }
     return html;
-  }
+  };
 
   this.rmc = function() // Rich Media Content
   {
     var html = "";
-    if(this.media){
-      var parts = this.media.split(".")
-      if (parts.length === 1) { this.media += ".jpg" } // support og media uploads
-      html += "<img class='media' src='"+this.dat+"/media/content/"+this.media+"'/>";
+    if (this.media) {
+      var parts = this.media.split(".");
+      if (parts.length === 1) {
+        this.media += ".jpg";
+      } // support og media uploads
+      html +=
+        "<img class='media' src='" +
+        this.dat +
+        "/media/content/" +
+        this.media +
+        "'/>";
     }
     return html;
-  }
+  };
 
-  this.rune = function()
-  {
-    if(this.whisper){
+  this.rune = function() {
+    if (this.whisper) {
       return "&";
     }
-    if(this.quote){
+    if (this.quote) {
       return "+";
     }
-    if(this.target){
+    if (this.target) {
       return ">";
     }
     return "";
-  }
+  };
 
-  this.formatter = function(message)
-  {
+  this.formatter = function(message) {
     var m = message;
 
     m = this.escape_html(m);
@@ -88,97 +145,108 @@ function Entry(data)
     m = this.format_style(m);
 
     return m;
-  }
+  };
 
-  this.escape_html = function(m)
-  {
+  this.escape_html = function(m) {
     return m
       .replace(/&/g, "&amp;")
       .replace(/</g, "&lt;")
       .replace(/>/g, "&gt;")
       .replace(/"/g, "&quot;")
       .replace(/'/g, "&#039;");
-  }
+  };
 
-  this.format_links = function(m)
-  {
+  this.format_links = function(m) {
     var words = m.split(" ");
     var n = [];
-    for(id in words){
+    for (id in words) {
       var word = words[id];
-      if(word.substr(0,6) == "dat://"){
-        var compressed = word.substr(0,12)+".."+word.substr(word.length-3,2);
-        n.push("<a href='"+word+"'>"+compressed+"</a>");
-      }
-      else if(word.substr(0,1) == "#"){
-        n.push("<c class='hashtag' data-operation='filter "+word+"'>"+word+"</c>");
-      }
-      else if (word.search(/^https?:\/\//) != -1) {
+      if (word.substr(0, 6) == "dat://") {
+        var compressed =
+          word.substr(0, 12) + ".." + word.substr(word.length - 3, 2);
+        n.push("<a href='" + word + "'>" + compressed + "</a>");
+      } else if (word.substr(0, 1) == "#") {
+        n.push(
+          "<c class='hashtag' data-operation='filter " +
+            word +
+            "'>" +
+            word +
+            "</c>"
+        );
+      } else if (word.search(/^https?:\/\//) != -1) {
         try {
-          var url = new URL(word)
-          var compressed = word.substr(word.indexOf("://")+3,url.hostname.length + 15)+"..";
-          n.push("<a href='"+url.href+"'>"+compressed+"</a>");
-        } catch(e) {
+          var url = new URL(word);
+          var compressed =
+            word.substr(word.indexOf("://") + 3, url.hostname.length + 15) +
+            "..";
+          n.push("<a href='" + url.href + "'>" + compressed + "</a>");
+        } catch (e) {
           console.error("Error when parsing url:", word, e);
           n.push(word);
         }
-      }
-      else{
-        n.push(word)
+      } else {
+        n.push(word);
       }
     }
     return n.join(" ").trim();
-  }
+  };
 
-  this.highlight_portal = function(m)
-  {
-    return m.replace('@'+r.portal.data.name,'<t class="highlight">@'+r.portal.data.name+"</t>")
-  }
+  this.highlight_portal = function(m) {
+    return m.replace(
+      "@" + r.portal.data.name,
+      '<t class="highlight">@' + r.portal.data.name + "</t>"
+    );
+  };
 
-  this.link_portals = function(m)
-  {
+  this.link_portals = function(m) {
     var words = m.split(" ");
     var n = [];
-    for(id in words){
+    for (id in words) {
       var word = words[id];
-      var name_match = r.operator.name_pattern.exec(word)
-      if(name_match && r.feed.portals[name_match[1]]){
-        var remnants = word.substr(name_match[0].length)
-        n.push("<a href='"+r.feed.portals[name_match[1]].dat+"' class='known_portal'>"+name_match[0]+"</a>"+remnants);
-      }
-      else{
-        n.push(word)
+      var name_match = r.operator.name_pattern.exec(word);
+      if (name_match && r.feed.portals[name_match[1]]) {
+        var remnants = word.substr(name_match[0].length);
+        n.push(
+          "<a href='" +
+            r.feed.portals[name_match[1]].dat +
+            "' class='known_portal'>" +
+            name_match[0] +
+            "</a>" +
+            remnants
+        );
+      } else {
+        n.push(word);
       }
     }
     return n.join(" ").trim();
-  }
-  this.format_style = function(m)
-  {
-    if(m.indexOf("{*") > -1 && m.indexOf("*}") > -1){
-      m = m.replace('{*',"<b>").replace('*}',"</b>");
+  };
+  this.format_style = function(m) {
+    if (m.indexOf("{*") > -1 && m.indexOf("*}") > -1) {
+      m = m.replace("{*", "<b>").replace("*}", "</b>");
     }
-    if(m.indexOf("{_") > -1 && m.indexOf("_}") > -1){
-      m = m.replace('{_',"<i>").replace('_}',"</i>");
+    if (m.indexOf("{_") > -1 && m.indexOf("_}") > -1) {
+      m = m.replace("{_", "<i>").replace("_}", "</i>");
     }
-    return m
-  }
+    return m;
+  };
 
-  this.time_ago = function()
-  {
+  this.time_ago = function() {
     return timeSince(this.timestamp);
-  }
+  };
 
-  this.is_visible = function()
-  {
-    if(this.whisper && this.target != r.portal.data.dat && this.portal != r.portal.data.name){
+  this.is_visible = function() {
+    if (
+      this.whisper &&
+      this.target != r.portal.data.dat &&
+      this.portal != r.portal.data.name
+    ) {
       return false;
     }
     return true;
-  }
+  };
 }
 
-function timeSince(date)
-{
+function timeSince(date) {
   var seconds = Math.floor((new Date() - date) / 1000);
   var interval = Math.floor(seconds / 31536000);
 
@@ -209,5 +277,4 @@ function timeSince(date)
   return "seconds";
 }
 
-
-r.confirm("script","entry");
+r.confirm("script", "entry");
--- scripts/feed.js
+++ scripts/feed.js
@@ -1,24 +1,23 @@
-function Feed(feed_urls)
-{
+function Feed(feed_urls) {
   this.feed_urls = feed_urls;
-  this.el = document.createElement('div'); this.el.id = "feed";
+  this.el = document.createElement("div");
+  this.el.id = "feed";
 
   this.archives = [];
   this.portals = {};
   this.urls = {};
   this.filter = "";
 
-  this.install = function(el)
-  {
+  this.install = function(el) {
     el.appendChild(this.el);
 
-    this.el.innerHTML = "Fetching "+this.feed_urls.length+" feeds..";
+    this.el.innerHTML = "Fetching " + this.feed_urls.length + " feeds..";
 
-    for(id in this.feed_urls){
+    for (id in this.feed_urls) {
       var archive = new DatArchive(this.feed_urls[id]);
       var fileEvents = archive.createFileActivityStream();
-      fileEvents.addEventListener('changed', e => {
-        console.log("Automated update")
+      fileEvents.addEventListener("changed", e => {
+        console.log("Automated update");
         r.feed.update();
       });
       archive.gateway = this.feed_urls[id];
@@ -27,127 +26,179 @@ function Feed(feed_urls)
     this.archives.push(r.portal.archive);
 
     this.update();
-  }
+  };
 
-  this.update = async function()
-  {
+  this.update = async function() {
     this.get_entries();
-    setTimeout(function(){ r.portal.port_list_el.innerHTML = r.feed.get_feed_html(); }, 1000);
-  }
+    setTimeout(function() {
+      r.portal.port_list_el.innerHTML = r.feed.get_feed_html();
+    }, 1000);
+  };
 
-  this.get_entries = function()
-  {
+  this.get_entries = function() {
     var entries = [];
     var online_ports_count = 0;
 
-    var archive_promises = this.archives.map((archive) => (
+    var archive_promises = this.archives.map(archive =>
       this.get_feed(archive)
-        .then((feed_entries) => {
+        .then(feed_entries => {
           online_ports_count += 1;
           entries = entries.concat(feed_entries);
           this.debounced_sort_refresh(entries);
         })
-        .catch((e) => {
-          console.warn(`Unable to fetch, this feed appears to be offline: ${archive.url}`);
+        .catch(e => {
+          console.warn(
+            `Unable to fetch, this feed appears to be offline: ${archive.url}`
+          );
         })
-    ));
+    );
 
     Promise.all(archive_promises).then(() => {
-      console.log("DONE")
+      console.log("DONE");
       // Finished attempting to load all ports, maybe do something here?
     });
-  }
+  };
 
-  this.get_feed_html = function()
-  {
+  this.get_feed_html = function() {
     var feed_html = "";
-    for(name in r.feed.portals){
+    for (name in r.feed.portals) {
       var portal = r.feed.portals[name];
       var url = r.feed.urls[name];
-      var last_entry = portal.feed[portal.feed.length-1];
-      var is_active = last_entry ? Math.floor((new Date() - last_entry.timestamp) / 1000) : 999999;
-      var rune = portal.port.indexOf(r.portal.data.dat) > -1 || portal.dat === r.portal.data.dat ? "@" : "~";
+      var last_entry = portal.feed[portal.feed.length - 1];
+      var is_active = last_entry
+        ? Math.floor((new Date() - last_entry.timestamp) / 1000)
+        : 999999;
+      var rune =
+        portal.port.indexOf(r.portal.data.dat) > -1 ||
+        portal.dat === r.portal.data.dat
+          ? "@"
+          : "~";
 
-      if(!last_entry){ continue; }
-      if(is_active > 190000 && portal.name != r.portal.data.name){
-        feed_html += "<ln title='"+(timeSince(last_entry.timestamp))+"' class='dead' data-operation='un"+url+"'>"+rune+""+portal.name+"</ln>";
+      if (!last_entry) {
+        continue;
       }
-      else{
-        feed_html+= "<ln title='"+(timeSince(last_entry.timestamp))+"' class='"+(is_active < 150000 ? "active" : "inactive")+"'><a href='"+url+"'>"+rune+""+portal.name+"</a></ln>";
+      if (is_active > 190000 && portal.name != r.portal.data.name) {
+        feed_html +=
+          "<ln title='" +
+          timeSince(last_entry.timestamp) +
+          "' class='dead' data-operation='un" +
+          url +
+          "'>" +
+          rune +
+          "" +
+          portal.name +
+          "</ln>";
+      } else {
+        feed_html +=
+          "<ln title='" +
+          timeSince(last_entry.timestamp) +
+          "' class='" +
+          (is_active < 150000 ? "active" : "inactive") +
+          "'><a href='" +
+          url +
+          "'>" +
+          rune +
+          "" +
+          portal.name +
+          "</a></ln>";
       }
     }
     return feed_html;
-  }
+  };
 
-  this.get_feed = async function(archive)
-  {
-    return Promise.all([await archive.readFile('portal.json'), DatArchive.resolveName(archive.url)])
-    .then(values => {
+  this.get_feed = async function(archive) {
+    return Promise.all([
+      await archive.readFile("portal.json"),
+      DatArchive.resolveName(archive.url)
+    ])
+      .then(values => {
         var portal = JSON.parse(values[0]);
-        var url = "dat://"+values[1];
+        var url = "dat://" + values[1];
         // append slash to port entry so that .indexOf works correctly in other parts (e.g ~runes)
         portal.port = portal.port.map(function(portal_entry) {
-          if (portal_entry.slice(-1) !== "/") { portal_entry += "/";}
-          return portal_entry
-        })
+          if (portal_entry.slice(-1) !== "/") {
+            portal_entry += "/";
+          }
+          return portal_entry;
+        });
         this.portals[portal.name] = portal;
         this.urls[portal.name] = url;
         return portal.feed
-          .filter((entry) => {
+          .filter(entry => {
             if (!this.filter) return true;
-            if ("@"+portal.name === this.filter) return true;
-            return entry.message.toLowerCase().includes(this.filter.toLowerCase());
+            if ("@" + portal.name === this.filter) return true;
+            return entry.message
+              .toLowerCase()
+              .includes(this.filter.toLowerCase());
           })
-          .map((entry, entry_id) => new Entry(
-            Object.assign({}, entry, {
-              portal: portal.name,
-              dat: archive.url,
-              id: entry_id,
-              seed: portal.port.indexOf(r.portal.data.dat) > -1 || portal.dat === r.portal.data.dat
-            })
-          ))
+          .map(
+            (entry, entry_id) =>
+              new Entry(
+                Object.assign({}, entry, {
+                  portal: portal.name,
+                  dat: archive.url,
+                  id: entry_id,
+                  seed:
+                    portal.port.indexOf(r.portal.data.dat) > -1 ||
+                    portal.dat === r.portal.data.dat
+                })
+              )
+          );
       })
-      .catch((e) => {
+      .catch(e => {
         console.error("Error reading remote portal.json; malformed json?", e);
-      })
-  }
+      });
+  };
 
-  this.debounced_sort_refresh = debounce(function(entries)
-  {
-    // Sort
-    var sorted_entries = entries.sort(function (a, b) {
-      return a.timestamp < b.timestamp ? -1 : 1;
-    });
+  this.debounced_sort_refresh = debounce(
+    function(entries) {
+      // Sort
+      var sorted_entries = entries.sort(function(a, b) {
+        return a.timestamp < b.timestamp ? -1 : 1;
+      });
 
-    this.refresh(sorted_entries.reverse());
-  }, 1000, false);
+      this.refresh(sorted_entries.reverse());
+    },
+    1000,
+    false
+  );
 
-  this.refresh = function(entries)
-  {
-    console.log("Refresh!")
+  this.refresh = function(entries) {
+    console.log("Refresh!");
 
-    var html = this.filter ? "<c class='clear_filter' data-operation='clear_filter'>Filtering by "+this.filter+"</c>" : "";
+    var html = this.filter
+      ? "<c class='clear_filter' data-operation='clear_filter'>Filtering by " +
+        this.filter +
+        "</c>"
+      : "";
     var c = 0;
-    for(id in entries){
+    for (id in entries) {
       var entry = entries[id];
-      if(!entry || entry.timestamp > new Date()) { continue; }
-      if(!entry.is_visible()){ continue; }
+      if (!entry || entry.timestamp > new Date()) {
+        continue;
+      }
+      if (!entry.is_visible()) {
+        continue;
+      }
       html += entry.to_html();
-      if(c > 40){ break; }
+      if (c > 40) {
+        break;
+      }
       c += 1;
     }
 
-    html += "<div class='entry'><t class='portal'>$rotonde</t><t class='timestamp'>Just now</t><hr/><t class='message' style='font-style:italic'>Welcome to #rotonde, a decentralized social network. Share your dat:// url with others and add theirs into the input bar to get started.</t></div>"
+    html +=
+      "<div class='entry'><t class='portal'>$rotonde</t><t class='timestamp'>Just now</t><hr/><t class='message' style='font-style:italic'>Welcome to #rotonde, a decentralized social network. Share your dat:// url with others and add theirs into the input bar to get started.</t></div>";
     this.el.innerHTML = html;
-  }
+  };
 }
 
 // See https://davidwalsh.name/javascript-debounce-function
-function debounce(func, wait, immediate)
-{
+function debounce(func, wait, immediate) {
   var timeout;
   return function() {
-    var context = this, args = arguments;
+    var context = this,
+      args = arguments;
     var later = function() {
       timeout = null;
       // if (!immediate) func.apply(context, args);
@@ -158,17 +209,16 @@ function debounce(func, wait, immediate)
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
   };
-};
-
-function portal_from_hash(hash)
-{
-  for(name in r.feed.portals){
-    var portal = r.feed.portals[name];
-    if(r.feed.portals[name].dat == hash){
-      return "@"+portal.name;
-    }
-  }
-  return hash.substr(0,12)+".."+hash.substr(hash.length-3,2);
 }
 
-r.confirm("script","feed");
+function portal_from_hash(hash) {
+  for (name in r.feed.portals) {
+    var portal = r.feed.portals[name];
+    if (r.feed.portals[name].dat == hash) {
+      return "@" + portal.name;
+    }
+  }
+  return hash.substr(0, 12) + ".." + hash.substr(hash.length - 3, 2);
+}
+
+r.confirm("script", "feed");
