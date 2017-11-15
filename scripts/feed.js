7231b70b2430703cf915e5010580d4d6c19110d1
Maik Macho <nuda1998@gmail.com>
2017-11-15 22:43:33 +0100

    Fix refresh on activity

--- scripts/feed.js
+++ scripts/feed.js
@@ -101,9 +101,13 @@ function Feed(feed_urls)
     }
 
     this.portals.push(portal);
-    var activity = portal.archive.createFileActivityStream("portal.json");
-    activity.addEventListener('changed', e => {
-      r.home.feed.refresh(portal.json.name+" changed");
+    var activity = portal.archive.createFileActivityStream();
+    activity.addEventListener("invalidated", e => {
+      r.home.feed.refresh(portal.json.name+" invalidated");
+      portal.refresh().then(() => {
+        r.home.update();
+        r.home.feed.refresh(portal.json.name+" refreshed");
+      });
     });
     r.home.update();
     r.home.feed.refresh(portal.json.name+" registered");
