71ffb003c87c448b4e693aa57b8ac7883d71cb83
Maik Macho <nuda1998@gmail.com>
2018-07-17 16:26:40 +0200

    Improve fetch-render loop once again

--- scripts/feed.js
+++ scripts/feed.js
@@ -400,7 +400,7 @@ Right now, restoring and improving the core experience is the top priority.
 
     this.preloader = ctx.add("preloader", ++eli, el => el || rd$`<div class="entry pseudo"  *?${rdh.toggleClass("done")}><div class="preloader"></div><div class="preloader b"></div></div>`);
     // TODO: Fetch feed tail outside of feed render!
-    if (fetched && this._fetchesWithoutUpdates < 4) {    
+    if (!fetched || this._fetchesWithoutUpdates < 2) {    
       this.fetchFeed(false, true);
     }
 
